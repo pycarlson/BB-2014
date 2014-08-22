e3aa79e70255ae3a18bbededc7d41d688b4bdd40
Paulette Luftig <pyluftig@gmail.com>
2014-08-21 21:56:57 -0700

    added mailer details to production file

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,10 +2,10 @@ ActionMailer::Base.smtp_settings = {
 :enable_starttls_auto => true,
 :address => 'smtp.gmail.com',
 :port => 587,
-:domain => 'brighterbeginnings.com',
+:domain => 'http://adopt-a-family-program.herokuapp.com/',
 :authentication => :plain,
 :user_name => ENV["P_EMAIL"],
 :password => ENV["P_PASSWORD"]
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "http://adopt-a-family-program.herokuapp.com/"
