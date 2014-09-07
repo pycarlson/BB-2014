0909dc3ff4d59a5c7a3ac880c683afbde7a1bfe6
Paulette Luftig <pyluftig@gmail.com>
2014-09-07 12:23:59 -0700

    add regex to family count on super

--- app/helpers/family_helper.rb
+++ app/helpers/family_helper.rb
@@ -3,5 +3,15 @@ module FamilyHelper
   def has_been_adopted?
     self.adopted == true
   end
+
+  def family_count(families)
+    if families.count == 1
+      "#{families.count} Family" 
+    elsif families.count > 0
+      "#{families.count} Families" 
+    else
+      "No Familes"
+    end 
+  end
   
 end
