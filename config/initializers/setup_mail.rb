f0fa8a2c3e47d41de23388a4b0702cbb7397e039
Paulette Luftig <pyluftig@gmail.com>
2014-10-20 19:41:57 -0700

    added new gmail account for return mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ ActionMailer::Base.smtp_settings = {
 :port => 587,
 :domain => 'http://adopt-a-family-program.herokuapp.com/',
 :authentication => :plain,
-:user_name => ENV["P_EMAIL"],
-:password => ENV["P_PASSWORD"]
+:user_name => ENV["BB_EMAIL"],
+:password => ENV["BB_PASSWORD"]
 }
 
 ActionMailer::Base.default_url_options[:host] = "http://adopt-a-family-program.herokuapp.com/"
